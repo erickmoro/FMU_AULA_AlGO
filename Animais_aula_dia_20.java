67f2d78287bea707ddcfa39c4b1291d0fb1d9e87
Erick <58540982+erickmoro@users.noreply.github.com>
2024-05-20 09:22:47 -0300

    Add files via upload

---
+++ Animais_aula_dia_20.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package aula_1;
+
+/**
+ *
+ * @author profsl
+ */
+public abstract class Animais {
+
+   String nome;
+   String cor;
+   int idade;
+   double peso;
+   boolean aquático;
+   
+    
+   
+   public static void main(String[] args) {
+     
+        Animais cachorro = new Animais();
+        
+
+    }
+    
+}
